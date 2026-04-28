603d2d1d9ac44d23494b47e6d07ad19df8ef2b48
Océane Leite <165897913+ocea17@users.noreply.github.com>
2026-04-28 13:05:36 +0200

    feat: ajout pages Home, Services, Réalisations, Mentions légales

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <HeroSection />
+    <ServicesSection />
+    <AboutSection />
+    <RealisationsSection />
+    <ContactSection />
+  </main>
+</template>
+
+<script setup>
+import HeroSection         from '@/components/HeroSection.vue'
+import ServicesSection     from '@/components/ServicesSection.vue'
+import AboutSection        from '@/components/AboutSection.vue'
+import RealisationsSection from '@/components/RealisationsSection.vue'
+import ContactSection      from '@/components/ContactSection.vue'
+</script>
