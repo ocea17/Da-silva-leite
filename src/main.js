503d6acc3a112895a765526d864ab45ddb25cebd
Océane Leite <165897913+ocea17@users.noreply.github.com>
2026-05-04 20:57:55 +0200

    chore(main): update main.js following asset and component refactor

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import router from './router'
 import App from './App.vue'
 import './assets/css/main.css'
 
+// ─── Easter egg console ───
+console.log(
+  '%c Salut !',
+  'color:#6DC319;font-size:20px;font-weight:bold;'
+)
+console.log(
+  '%cVous êtes curieux ? C\'est bien ! N\'hésitez pas à explorer le code et mon site web : oceaneleite.fr !',
+  'color:#6DC319;font-size:13px;'
+)
+console.log(
+  '%cSi vous voulez me contacter : oceane.leitepro@gmail.com',
+  'color:#6DC319;font-size:13px;font-style:italic;'
+)
+
 const app = createApp(App)
 
 app.use(MotionPlugin)
